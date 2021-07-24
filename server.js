2d67ea7c1d2949959e396121e6d9d86fdba34834
hugosmoreira <hugo.n4@gmail.com>
2021-07-23 21:08:30 -0700

    Newsletter node

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const request = require("request");
+const async = require("async");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res, next) => {
+  res.render("main/home");
+});
+
+app.listen(3030, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Running on Port 3030");
+  }
+});
